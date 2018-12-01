81eddda4a0922b0fc4bb2b04a301dc058d30c928
Jose M Sallan <jose.maria.sallan@upc.edu>
2018-12-01 11:30:57 +0100

    anàlisi de NAs

--- Analisi01.R
+++ Analisi01.R
@@ -1,4 +1,4 @@
-setwd("~/Documents/ContractesMenors")
+setwd("~/Documents/ContractesMenors") #canviar per adreça a cada ordinador
 
 
 library(dplyr)
@@ -8,7 +8,38 @@ library(lubridate)
 #----llegint dades----
 
 contractes <- readRDS("data/contractes.RDS")
+
+#----examen previ---
+
 str(contractes)
+unique(contractes$Tipus)
+
+contractes <- contractes %>% mutate(Tipus = sub("Gestió de Serveis Públics", "Gestió de serveis públics", Tipus))
+
+unique(contractes$Organ)
+unique(contractes$Adjudicatari)
+
+#contractes amb NAs
+contractes %>% filter(is.na(Import))
+contractes %>% filter(is.na(Tipus))
+contractes %>% filter(is.na(Organ))
+contractes %>% filter(is.na(Adjudicatari))
+contractes %>% filter(is.na(Objecte))
+contractes %>% filter(is.na(Import)) #120 contractes sense import
+
+#contractes als que falta l'import
+contractes %>% filter(is.na(Import)) %>% group_by(Tipus, Organ) %>% summarise(NombreContractes=n())
+contractes %>% filter(is.na(Import)) %>% group_by(Organ, Adjudicatari) %>% summarise(NombreContractes=n())
+
+#total contractes
+contractes %>% summarise(total=sum(Import, na.rm = TRUE), mitjana=mean(Import, na.rm = TRUE))
+
+contractes %>% group_by(Tipus) %>% summarise(total=sum(Import, na.rm = TRUE), mitjana=mean(Import, na.rm = TRUE))
+
+contractes %>% group_by(Organ) %>% summarise(total=sum(Import, na.rm = TRUE), mitjana=mean(Import, na.rm = TRUE))
+
+contractes %>% group_by(Any) %>% summarise(total=sum(Import, na.rm = TRUE), mitjana=mean(Import, na.rm = TRUE))
+
 
 #----distribució de contractes per import----
 
@@ -18,8 +49,6 @@ contractes %>% filter(Import < 20000) %>% ggplot(aes(Import)) + geom_density() +
 
 #----nombre de contractes per mes---
 
-contractes %>% group_by(year(Data), month(Data)) %>% summarise(num_contractes=n()) %>% arrange(desc(num_contractes))
-
+contractes %>% group_by(Data) %>% summarise(num_contractes=n()) %>% arrange(desc(num_contractes))
+contractes %>% group_by(Data) %>% summarise(total_import=sum(Import)) %>% arrange(desc(total_import))
 contractes %>% group_by(Data) %>% summarise(num_contractes=n()) %>% ggplot(aes(Data, num_contractes)) + geom_line()
-
-contractes %>% group_by(Data) %>% summarise(num_contractes=n()) %>% filter(num_contractes > 1000)
